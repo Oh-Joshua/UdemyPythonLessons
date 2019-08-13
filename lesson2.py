153889de01b9f9f1d9e17f1efb1d39dd5f70ff02
oh-joshua <yosyu.o@gmail.com>
2019-08-13 12:01:32 +0900

    Srings and characters.

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+# 文字列の勉強
+
+print('Hello')              # 文字列はシングル・クォート
+print("Hello")              # ダブル・クオートもＯＫ
+
+print("I don't know")       # ダブル・クォートの中にシングル・クオートが入れられる
+print('I don\'t know')      # エスケープは'\'
+print('say "I don\'t know"')
+                            # シングル・クオートの中にダブル・クオートを入れられる
+print("say \"I don\'t know\"")
+                            # ダブル・クオートの中でダブル・クオートを使うときは、エスケープする
+print('Hello.\nHow are you?')
+                            # 改行は'\n'
+
