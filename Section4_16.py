0e34d83fc7f6f807cbfeb27030308c265588f138
oh-joshua <yosyu.o@gmail.com>
2019-08-14 12:08:16 +0900

    19.リストのコピー

--- Section4_16.py
+++ Section4_16.py
@@ -76,3 +76,106 @@ a.extend(b)         # extend:リスとをつなげる
 print(a)
 
 #18.リストのメソッド
+print('===================================')
+r = [1, 2, 3, 4, 5, 1, 2, 3]
+print(r.index(3))   # index:リストの中で3がある場所を返す
+print(r.index(3, 3))
+                    # index:リストの3番目以降で3がある場所を捜す
+print(r.index(1, 2))
+                    # index:リストの2番目以降で1がある場所を捜す
+print(r.count(3))   # count:3の件数をカウントする
+
+if 5 in r:          # もし、rのリスとに5がある場合
+    print('exist')
+
+if 100 in r:        # もし、rのリスとに100がある場合
+    print('exist')
+
+r.sort()            # リストrの要素をソートする
+print(r)
+r.sort(reverse=True)
+                    # リストrを逆順にそーとする
+print(r)
+r.reverse()         # reverse:リストrを逆に並べる
+print(r)
+
+print('-----------------------------------')
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9, 10]
+print(a)
+print(b)
+x = a + b
+print(x)
+
+x.reverse()
+print(x)
+
+# ネストした配列
+a = ['a', 'b', 'c'] # 文字のリスト
+n = [1, 2, 3]       # 数字のリスト
+x = [a, n]          # 二つのリストをリストにする
+print(x)
+a.reverse()
+n.reverse()
+x = [a, n]          # 二つのリストをリストにする
+print(x)
+# ネストした配列
+x = [a, n]          # 二つのリストをリストにする
+
+s = 'My name is Mike'
+                    # 文字列を定義する
+to_split = s.split(' ')
+                    # 区切り文字' 'でリストに代入する
+print(s)
+print(to_split)
+
+x = ' '.join(to_split)
+                    # 区切り文字' 'でリストを結合する
+print(x)
+x = '----'.join(to_split)
+                    # 区切り文字'----'でリストを結合する
+print(x)
+
+# print(help(list))   # help関数でリストのヘルプを表示できる
+
+# 19.リストのコピー
+print('===================================')
+i = [1, 2, 3, 4, 5] # リストiを作る
+j = i               # リストjにiをコピーする
+print('i = ', i)
+print('j = ', j)
+# iの最初を100に書き換えると、jの最初も書き換わる
+# iのポインターを渡しているため
+i[0] = 100
+print('i = ', i)
+print('j = ', j)
+
+print('-----------------------------------')
+x = [1, 2, 3, 4, 5] # リストxを作る
+y = x.copy()        # リストの要素をコピーする
+y = x[:]            # リストの要素をコピーする(この書き方でも同じ動きになる# )
+y[0] = 100          # リストyの0番目に100を代入する
+print('x = ', x)    # xの値は変らない
+print('y = ', y)    # yの値だけ変わる
+
+# リストの値渡しと参照渡し
+X = 20
+Y = X               # 数値データの場合、値を代入する
+print(X)
+print(Y)
+Y = 5
+print(X)
+print(Y)
+
+# XとYは、違うobjectになっている
+print(id(X))
+print(id(Y))
+
+X = ['a', 'b']
+Y = X               # 配列の場合、object参照を渡す
+Y[0] = 'p'
+print(X)
+print(Y)
+# XとYは同じobjectになっている
+print(id(X))
+print(id(Y))
